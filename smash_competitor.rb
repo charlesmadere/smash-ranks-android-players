776a26c29160ea3cca41ef7dda1b084584eaafbd
Charles Madere <charlesrmadere@gmail.com>
2018-05-20 03:04:17 -0700

    lots of small smash_competitor fixes

--- smash_competitor.rb
+++ smash_competitor.rb
@@ -1,4 +1,5 @@
 require "uri"
+require_relative "utils.rb"
 
 GAR_PR_HOST = "garpr.com"
 NOT_GAR_PR_HOST = "notgarpr.com"
@@ -6,47 +7,68 @@ NOT_GAR_PR_HOST = "notgarpr.com"
 class SmashCompetitor
 
 	def initialize(splits)
-		@garPrUrl = splits[1]
-		@notGarPrUrl = splits[2]
-		@tag = splits[3]
-		@realName = splits[4]
-		@main1 = splits[5]
-		@main2 = splits[6]
-		@main3 = splits[7]
-		@twitterUrl = splits[8]
-		@twitchUrl = splits[9]
-		@youtubeUrl = splits[10]
-		@otherUrl = splits[11]
-		@avatar = splits[12]
+		@garPrUrl = strip_string_quotes(splits[1]).downcase
+		@notGarPrUrl = strip_string_quotes(splits[2]).downcase
+		@tag = strip_string_quotes(splits[3])
+		@realName = strip_string_quotes(splits[4])
+		@main1 = strip_string_quotes(splits[5])
+		@main2 = strip_string_quotes(splits[6])
+		@main3 = strip_string_quotes(splits[7])
+		@twitterUrl = strip_string_quotes(splits[8])
+		@twitchUrl = strip_string_quotes(splits[9])
+		@youtubeUrl = strip_string_quotes(splits[10])
+		@otherUrl = strip_string_quotes(splits[11])
+		@avatar = strip_string_quotes(splits[12])
 	end
+
+	def gar_pr_id
+		return get_id_from_gar_pr_url(@garPrUrl)
+	end
+
+	def get_id_from_gar_pr_url(url)
+		if url == nil || url.empty?
+			return nil
+		end
+
+		# TODO
+	end
+	private :get_id_from_gar_pr_url
 
 	def is_gar_pr_url_valid(url, host)
 		# example GAR PR player profile URL: https://www.garpr.com/#/norcal/players/5877eb55d2994e15c7dea982
 		# example Not GAR PR player profile URL: https://www.notgarpr.com/#/newjersey/players/545c854e8ab65f127805bd6f
 
-		if url == nil
+		if url == nil || url.empty?
 			return false
 		end
 
 		uri = URI.parse(url)
 
-		if uri.host != host
+		if !uri.host.end_with?(host)
 			return false
 		end
 
 		path = uri.path
-		puts path
+
+		# TODO
 
 		return true
 	end
 	private :is_gar_pr_url_valid
 
-	def is_valid
-		return is_gar_pr_url_valid(@garPrUrl, GAR_PR_HOST) && is_gar_pr_url_valid(@notGarPrUrl, NOT_GAR_PR_HOST)
+	def is_valid?
+		return (is_gar_pr_url_valid(@garPrUrl, GAR_PR_HOST) ||
+						is_gar_pr_url_valid(@notGarPrUrl, NOT_GAR_PR_HOST)) &&
+				@tag != nil && !@tag.empty? &&
+				@realName != nil && !@realName.empty?
+	end
+
+	def not_gar_pr_id
+		return get_id_from_gar_pr_url(@notGarPrUrl)
 	end
 
 	def to_json
-		if !is_valid
+		if !is_valid?
 			raise "This SmashCompetitor (#{tag}) is not valid! Refusing to output as JSON."
 		end
 
