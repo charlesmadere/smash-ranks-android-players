5d656f545ce4731d486ec1eaecbb531f80360ab1
Charles Madere <charlesrmadere@gmail.com>
2018-05-19 21:14:23 -0700

    general progress in parsing lines from the CSV file

--- GenerateRoster.rb
+++ GenerateRoster.rb
@@ -1,19 +1,33 @@
 #!/usr/bin/ruby
 
+PROPER_SPLITS_LENGTH = 13
 readFirstLine = false
-players = Array.new
+smashCompetitors = Array.new
 
-File.open("GAR PR Player Roster.csv").each do |line|
+File.open("GAR PR Player Roster.csv").each_with_index do |line, index|
 	if readFirstLine
-		# Attempt to read a line into a Player object
-		# If null is returned, then we know that this Player object is invalid (it's missing
-		# some necessary fields / properties)
-		# If null is not returned, add this Player to the players array
+		splits = line.split(',')
+		smashCompetitor = nil
+
+		if splits.length == PROPER_SPLITS_LENGTH
+			smashCompetitor = SmashCompetitor.initialize(splits)
+		end
+
+		if smashCompetitor == nil
+			puts "Smash Competitor at line #{index} has some data error(s)"
+		else
+			smashCompetitors.push(smashCompetitor)
+		end
+
+		# Attempt to read a line into a SmashCompetitor object
+		# If nil is returned, then we know that this SmashCompetitor object is invalid (it's
+		# missing some necessary fields / properties)
+		# If nil is not returned, add this SmashCompetitor to the players array
 	else
 		readFirstLine = true
 	end
 end
 
-# Write out the players as JSON into a simple text file
+# Write out the smashCompetitors as JSON into a simple text file
 
-puts "wrote out #{players.length} player(s)"
+puts "wrote out #{smashCompetitors.length} player(s)"
