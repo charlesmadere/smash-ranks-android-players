765a252eb18c4ccbe070573a98ff6984d956880b
Charles Madere <charlesrmadere@gmail.com>
2018-05-28 14:05:45 -0700

    smash characters array

--- smash_character.rb
+++ smash_character.rb
@@ -1,5 +1,65 @@
-class SmashCharacter
-
-
-
-end
+SMASH_CHARACTERS = [
+    "byo", # Bayonetta
+    "bow", # Bowser
+    "bjr", # Bowser Jr.
+    "fcn", # Captain Falcon
+    "olm", # Captain Olimar
+    "cld", # Cloud
+    "crn", # Corrin
+    "dpt", # Dark Pit
+    "ddy", # Diddy Kong
+    "doc", # Doctor Mario
+    "dnk", # Donkey Kong
+    "dck", # Duck Hunt
+    "fco", # Falco
+    "fox", # Fox
+    "gnn", # Ganondorf
+    "grn", # Greninja
+    "ics", # Ice Climbers
+    "ike", # Ike
+    "puf", # Jigglypuff
+    "ddd", # King Dedede
+    "kby", # Kirby
+    "lnk", # Link
+    "lmc", # Little Mac
+    "lcr", # Lucario
+    "lcn", # Lucina
+    "lgi", # Luigi
+    "mar", # Mario
+    "mrt", # Marth
+    "meg", # Megaman
+    "mtk", # Meta Knight
+    "mw2", # Mewtwo
+    "mib", # Mii Brawler
+    "mig", # Mii Gunner
+    "mis", # Mii Swordfighter
+    "gnw", # Mr. Game & Watch
+    "nes", # Ness
+    "pac", # Pac-Man
+    "pal", # Palutena
+    "pch", # Peach
+    "pic", # Pichu
+    "pik", # Pikachu
+    "pit", # Pit
+    "pkt", # Pokémon Trainer
+    "rob", # R.O.B.
+    "rbn", # Robin
+    "ros", # Rosalina
+    "roy", # Roy
+    "ryu", # Ryu
+    "sam", # Samus
+    "shk", # Sheik
+    "slk", # Shulk
+    "snk", # Snake
+    "snc", # Sonic
+    "sqt", # Squirtle
+    "tlk", # Toon Link
+    "vlg", # Villager
+    "war", # Wario
+    "wft", # Wii Fit Trainer
+    "wlf", # Wolf
+    "ysh", # Yoshi
+    "ylk", # Young Link
+    "zld", # Zelda
+    "zss" # Zero Suit Samus
+]
