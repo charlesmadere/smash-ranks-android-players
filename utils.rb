d837a7de3bbc1a28020708785e60e2f1d856c0a0
Charles Madere <charlesrmadere@gmail.com>
2018-05-31 22:04:08 -0700

    more pre CSV code removed

--- utils.rb
+++ utils.rb
@@ -1,24 +1,7 @@
 require "json"
 
 PATH = "json"
-
-def strip_string_quotes(string)
-	if string == nil || string.empty?
-		return string
 	end
-
-	string = string.strip
-
-	if string[0] == "\""
-		string = string[1, string.length - 1]
-	end
-
-	if string[string.length - 1] == "\""
-		string = string[0, string.length - 1]
-	end
-
-	return string.strip
-end
 
 def write_competitors_to_json_file(competitorsHash, fileName)
 	fileDirectory = "#{PATH}"
