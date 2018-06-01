5197f2b1bf3a5d4de30eb4c15ea14b2be0acc66f
Charles Madere <charlesrmadere@gmail.com>
2018-05-31 22:13:05 -0700

    added some comments to generate_roster

--- generate_roster.rb
+++ generate_roster.rb
@@ -11,12 +11,16 @@ NOT_GAR_PR_FILE_PATH = "json#{File::SEPARATOR}"
 PROPER_ROW_LENGTH = 13
 ROSTER_FILE_NAME = "GAR PR Player Roster.csv"
 
-smashCompetitors = Array.new
 
 if !File.exist?(ROSTER_FILE_NAME)
 	puts "Roster file (#{ROSTER_FILE_NAME}) does not exist!"
 	return
 end
+
+
+# This section reads in player data from the CSV file
+
+smashCompetitors = Array.new
 
 CSV.foreach(ROSTER_FILE_NAME, headers: true, skip_blanks: true) do |row|
 	smashCompetitor = nil
@@ -34,6 +38,9 @@ end
 
 puts "Read in #{smashCompetitors.length} player(s)."
 
+
+# This section creates a Hash of valid GAR PR players and builds up their data into JSON
+
 garPrCompetitorsHash = Hash.new
 smashCompetitors.each do |smashCompetitor|
 	garPrId = smashCompetitor.gar_pr_id
@@ -43,9 +50,13 @@ smashCompetitors.each do |smashCompetitor|
 	end
 end
 
+
+# This section writes out the GAR PR players to a local JSON file
 write_competitors_to_json_file(garPrCompetitorsHash, GAR_PR_FILE_PATH, GAR_PR_FILE_NAME)
 puts "Wrote out #{garPrCompetitorsHash.length} GAR PR player(s) to #{GAR_PR_FILE_PATH}#{GAR_PR_FILE_NAME}"
 
+
+# This section creates a Hash of valid Not GAR PR players and builds up their data into JSON
 
 notGarPrCompetitorsHash = Hash.new
 smashCompetitors.each do |smashCompetitor|
@@ -56,5 +67,6 @@ smashCompetitors.each do |smashCompetitor|
 	end
 end
 
+# This section writes out the Not GAR PR players to a local JSON file
 write_competitors_to_json_file(notGarPrCompetitorsHash, NOT_GAR_PR_FILE_PATH, NOT_GAR_PR_FILE_NAME)
-puts "Wrote out #{notGarPrCompetitorsHash.length} GAR PR player(s) to #{NOT_GAR_PR_FILE_PATH}#{NOT_GAR_PR_FILE_NAME}"
+puts "Wrote out #{notGarPrCompetitorsHash.length} Not GAR PR player(s) to #{NOT_GAR_PR_FILE_PATH}#{NOT_GAR_PR_FILE_NAME}"
